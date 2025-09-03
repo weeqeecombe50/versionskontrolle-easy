1db8d1deca4eff24ed3a2acf0f970765b9545768
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-03 20:08:25 +0300

    Added a comment to clarify example commands usage.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const VersionskontrolleEasy = require('./vce');
 
 const vce = new VersionskontrolleEasy();
 
-// Example commands
+// Example commands to demonstrate version control functionalities
 vce.init('mein-projekt');
 vce.track('meine-datei.txt');
 vce.commit('Meine erste Änderung');
