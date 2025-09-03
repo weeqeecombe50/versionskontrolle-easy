00046908f1053351bec238db62506463cb607f22
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-03 20:06:01 +0300

    feat: initial commit for vce.js

---
+++ vce.js
@@ -0,0 +1,28 @@
+// Hauptfunktionalität des Versionskontrolle Easy
+
+class VersionskontrolleEasy {
+    constructor() {
+        this.projekte = {};
+    }
+
+    init(projektName) {
+        if (this.projekte[projektName]) {
+            console.log(`Projekt '${projektName}' existiert bereits.`);
+        } else {
+            this.projekte[projektName] = { dateien: [], änderungen: [] };
+            console.log(`Projekt '${projektName}' erfolgreich initialisiert.`);
+        }
+    }
+
+    track(datei) {
+        // Logik zum Verfolgen von Änderungen
+        console.log(`Überwachung der Datei: ${datei}`);
+    }
+
+    commit(message) {
+        // Logik zum Speichern von Änderungen
+        console.log(`Änderungen mit der Nachricht '${message}' gespeichert.`);
+    }
+}
+
+module.exports = VersionskontrolleEasy;
